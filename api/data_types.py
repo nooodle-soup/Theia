82b2b5b28bb9df8792b2eae8160aafa71db352ac
Vineet Agarwal <vineetagarwal2402@gmail.com>
2024-02-06 12:43:44 +0530

    Improve validation and ruff format

--- api/data_types.py
+++ api/data_types.py
@@ -29,11 +29,22 @@ class GeoJson(BaseDataModel):
         coordinates = shape["coordinates"]
 
         if type == "MultiPolygon":
-            return cls(type=type, coordinates=[Coordinate(**point) for polygon in coordinates[0] for point in polygon])
+            return cls(
+                type=type,
+                coordinates=[
+                    Coordinate(**point)
+                    for polygon in coordinates[0]
+                    for point in polygon
+                ],
+            )
         elif type == "Polygon":
-            return cls(type=type, coordinates=[Coordinate(**point) for point in coordinates[0]])
+            return cls(
+                type=type, coordinates=[Coordinate(**point) for point in coordinates[0]]
+            )
         elif type == "LineString":
-            return cls(type=type, coordinates=[Coordinate(**point) for point in coordinates])
+            return cls(
+                type=type, coordinates=[Coordinate(**point) for point in coordinates]
+            )
         elif type == "Point":
             return cls(type=type, coordinates=[Coordinate(**coordinates)])
         else:
@@ -103,26 +114,46 @@ class SearchParams(BaseModel):
     latitude: float | None = Field(default=None)
     bbox: List[Coordinate] | None = Field(default=None)
     max_cloud_cover: int | None = Field(default=None)
+    min_cloud_cover: int | None = Field(default=None)
     start_date: str | None = Field(default=None)
     end_date: str | None = Field(default=None)
     months: List[int] | None = Field(default=None)
     max_results: int = Field(default=100)
 
+    @validator("start_date", pre=True, always=True)
+    def check_start_date(cls, v, values):
+        if "end_date" in values and values["end_date"] is not None and v is None:
+            raise ValueError("Start date must be provided when End date is provided")
+        return v
+
+    @validator("end_date", pre=True, always=True)
+    def check_end_date(cls, v, values):
+        if "start_date" in values and values["start_date"] is not None and v is None:
+            raise ValueError("End date must be provided when Start date is provided")
+        return v
+
     @validator("longitude", pre=True, always=True)
     def check_longitude(cls, v, values):
-        if "latitude" in values and v is None:
+        if "latitude" in values and values["latitude"] is not None and v is None:
             raise ValueError("Longitude must be provided when latitude is provided")
         return v
 
     @validator("latitude", pre=True, always=True)
     def check_latitude(cls, v, values):
-        if "longitude" in values and v is None:
+        if "longitude" in values and values["longitude"] is not None and v is None:
             raise ValueError("Latitude must be provided when longitude is provided")
         return v
 
     @validator("bbox", pre=True, always=True)
     def check_bbox(cls, v, values):
-        if "longitude" in values or "latitude" in values:
+        if (
+            "longitude" in values
+            and "latitude" in values
+            and values["longitude"] is not None
+            and values["latitude"] is not None
+        ):
             if v is not None:
-                raise ValueError("Either provide longitude and latitude or bbox, not both")
+                raise ValueError(
+                    "Either provide longitude and latitude or bbox, not both"
+                )
         return v
