2f920a1599a67907d44cab903e5864a5c62a345c
Vineet Agarwal <vineetagarwal2402@gmail.com>
2024-02-12 10:15:24 +0530

    Add function to create api object

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from dotenv import dotenv_values
 
+from api import theia_api
+
 config = {
     **dotenv_values(".env.shared"),  # load shared development variables
     **dotenv_values(".env.secret"),  # load sensitive variables
@@ -11,3 +13,10 @@ if config["USERNAME"] == "None":
 
 if config["PASSWORD"] == "None":
     pass
+
+
+def get_api():
+    assert config["PASSWORD"] is not None
+    assert config["USERNAME"] is not None
+    api = theia_api.TheiaAPI(username=config["USERNAME"], password=config["PASSWORD"])
+    return api
